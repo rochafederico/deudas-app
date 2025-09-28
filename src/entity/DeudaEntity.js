9702f3a672db9f74e867808f2c244f20fccd98b6
Federico Rocha <rochafederico@users.noreply.github.com>
2025-09-28 20:46:58 -0300

    feat: Update DeudaEntity constructor to include optional ID parameter for updates

--- src/entity/DeudaEntity.js
+++ src/entity/DeudaEntity.js
@@ -2,11 +2,15 @@
 export class DeudaEntity {
     /**
      * @param {Object} params
+     * @param {number} [params.id] - ID de la deuda (solo para updates)
      * @param {string} params.acreedor
      * @param {string} params.tipoDeuda
      * @param {string} [params.notas]
      */
-    constructor({ acreedor, tipoDeuda, notas = '' }) {
+    constructor({ id, acreedor, tipoDeuda, notas = '' }) {
+        if (id !== undefined) {
+            this.id = id;
+        }
         this.acreedor = acreedor;
         this.tipoDeuda = tipoDeuda;
         this.notas = notas;
